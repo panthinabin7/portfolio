e249473470f8b7190c913bc55e1f93234dc5537c
Nabin Panthi <panthinabin7@gmail.com>
2023-07-20 07:25:38 +0530

    assets added sucessfully

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -84,7 +84,7 @@ const Works = () => {
         </motion.p>
       </div>
 
-      <div className="mt-20 flex flex-wrap gap-7">
+      <div className="mt-20 flex flex-wrap gap-7 justify-evenly">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
--- src/constants/index.js
+++ src/constants/index.js
@@ -105,35 +105,35 @@ const projects = [
   {
     name: "E-Commerce",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Web-based platform that allows users to search, filter, and manage amount and clothing, providing a convenient and efficient solution for shopping needs.",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
+        name: "Strapi",
         color: "green-text-gradient",
       },
       {
-        name: "tailwind",
+        name: "scss",
         color: "pink-text-gradient",
       },
     ],
     image: carrent,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/panthinabin7/captive-mart_E-commerce",
   },
   {
-    name: "Job IT",
+    name: "Netflix",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      "Web application that enables users to create account and login to the site,and can view various genres like Orginals, Trending, Top Rated, Action, Comedy, Romance, Horror.",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "restapi",
+        name: "restapi (TMDb) ",
         color: "green-text-gradient",
       },
       {
@@ -142,28 +142,7 @@ const projects = [
       },
     ],
     image: jobit,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Trip Guide",
-    description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-    tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: tripguide,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/panthinabin7/netflix-clone",
   },
 ];
 
